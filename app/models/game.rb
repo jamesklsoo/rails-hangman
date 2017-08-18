31785a57b7fe6fe6005135c50dde08d61cc75e3a
James Soo <jamesklsoo@gmail.com>
2017-08-18 12:59:27 +0800

    update game and guess model

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,2 +1,53 @@
 class Game < ApplicationRecord
+  has_many :guesses, dependent: :destroy
+
+  before_validation :choose_a_word, on: :create
+
+  Letter = Struct.new(:char, :"guessed", :"guessable?") do
+    def to_s
+      char
+    end
+  end
+
+  def letters
+    word.chars.map do |c|
+      guessed = already_guessed?(c)
+      guessable = (c =~ /[a-z]/i && !already_guessed? (c))
+      Letter.new(c, guessed, guessable)
+    end
+  end
+
+  def initial_lives
+    4
+  end
+
+  def lives_remaining
+    initial_lives - wrong_guesses.size
+  end
+
+  def already_guessed?(letter)
+    guessed_letters.include?(letter.downcase)
+  end
+
+  def lost?
+    lives_remaining <= 0
+  end
+
+  def won?
+    letters.select(&:guessable?).empty?
+  end
+
+  private
+
+  def wrong_guesses
+    guessed_letters.select { |letter| !word.downcase.include?(letter.downcase)}
+  end
+
+  def guessed_letters
+    guesses.map(&:letter).map(&:downcase)
+  end
+
+  def choose_a_word
+    self.word ||= File.read(Rails.root + "config/words.txt").split(/\n/).sample
+  end
 end
--- app/models/guess.rb
+++ app/models/guess.rb
@@ -1,2 +1,3 @@
 class Guess < ApplicationRecord
+  validates :letter, uniqueness: { scope: :game_id}
 end
