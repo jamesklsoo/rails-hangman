3bcbfde1f623ae025bb9c8d1b87afe74e3e4a529
James Soo <jamesklsoo@gmail.com>
2017-08-18 14:53:07 +0800

    add game helper

--- app/helpers/games_helper.rb
+++ app/helpers/games_helper.rb
@@ -1,2 +1,10 @@
 module GamesHelper
+  def new_game_button
+    button_to "New game", games_url, method: :post, class: 'btn btn-primary'
+  end
+
+  def create_guess_button(game, letter, html_options={})
+    default_opts = {method: :post, disabled: game.already_guessed?(letter), data: {'disabled-with' => letter}}
+    button_to(letter, game_guesses_url(game, letter: letter), html_options.merge(default_opts))
+  end
 end
